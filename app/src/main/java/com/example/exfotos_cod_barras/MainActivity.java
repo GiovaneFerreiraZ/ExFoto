f7952c973c480313d5a0a75705a099f22afab886
GiovaneFerreiraZ <65687833+GiovaneFerreiraZ@users.noreply.github.com>
2020-06-03 20:20:14 -0300

    Tirador de Foto

---
+++ app/src/main/java/com/example/exfotos_cod_barras/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.exfotos_cod_barras;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnTiraFoto;
+    ImageView imagem;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        retiraFoto();
+    }
+    private void retiraFoto()
+    {
+        imagem = (ImageView)findViewById(R.id.imagem);
+        Button btnTiraFoto = (Button)findViewById(R.id.btnTiraFoto);
+        btnTiraFoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent,0);
+            }
+        });
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode,resultCode,data);
+        Bundle bundle = data.getExtras();
+
+        if(data != null)
+        {
+            Bitmap bitmap = (Bitmap)bundle.get("data");
+            imagem.setImageBitmap(bitmap);
+        }
+    }
+}
